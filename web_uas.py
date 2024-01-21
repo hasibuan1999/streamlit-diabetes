be8b6af482950b0f52d21185faf7bce2e7fb00a4
hasibuan1999 <142510883+hasibuan1999@users.noreply.github.com>
2024-01-21 10:40:49 +0700

    web_uas.py

--- web_uas.py
+++ web_uas.py
@@ -3,15 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.naive_bayes import GaussianNB
-from PIL import Image
-from streamlit_option_menu import option_menu
-import plotly.express as px
 
-with st.sidebar:
-    selected = option_menu('Aplikasi Klasifikasi Diabetes', ['Klasifikasi Diabetes', 'Keterangan Label Data Set'],
-                           icons=['house', 'gear'], menu_icon="cast", default_index=0)
-
-if (selected == 'Klasifikasi Diabetes'):
     st.title('Prediksi Terkena Diabetes')
     model = pickle.load(open('ramadhanpratamaputra_UTS.pkl', 'rb'))
 
@@ -35,30 +27,3 @@ if (selected == 'Klasifikasi Diabetes'):
         else :
             diabet_prediction = "Pasien Tidak Terkena Diabetes"
         st.success(diabet_prediction)
-
-
-if (selected == 'Keterangan Label Data Set'):
-    st.title('Keterangan Diabetes')
-    img = Image.open('diabetes.jpg')
-    st.image(img, use_column_width=False)
-    st.write("""
-         # 
-1. Pregnancies: Untuk menyatakan Jumlah kehamilan
-
-2. Glucose : Untuk menyatakan kadar Glukosa dalam darah
-
-3 BloodPressure Untuk menyatakan pengukuran tekanan darah
-
-4. SkinThickness: Untuk menyatakan ketebalan kulit
-
-5. Insulin: Untuk menyatakan kadar Insulin dalam darah
-
-6. BMI: Untuk menyatakan indeks massa tubuh
-
-7. DiabetesPedigreeFunction : Untuk menyatakan persentase Diabetes
-
-8. Age: Untuk menyatakan usia.
-
-9. Outcome :Untuk menyatakan hasil akhir 1 adalah Ya dan 0 adalah Tidak
-         """
-             )
