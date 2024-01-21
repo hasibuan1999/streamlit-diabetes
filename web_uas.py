57720a59b8c3b4ab46402780fb48857fbc504fde
hasibuan1999 <142510883+hasibuan1999@users.noreply.github.com>
2024-01-21 11:46:10 +0700

    web_uas.py

--- web_uas.py
+++ web_uas.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
-from sklearn.naive_bayes import GaussianNB
 
 model = pickle.load(open('ramadhanpratamaputra_UTS.pkl', 'rb'))
 st.title('Prediksi Terkena Diabetes')
